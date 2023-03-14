e725626daa5a93b9e065b8a7bf2b2e0ab0e6ddfe
Marwan Abdelnaby <marwan.magdy37@gmail.com>
2023-03-15 00:25:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Resource Managment APP Frontend
+
+
+## Default User Setup
+
+The initial database already contains some sample data and you can access it with the following users:
+
+ - **username:** user1@test.com
+ - **password:** Test@123
+
+---
+
+ - **username:** user2@test.com
+ - **password:** Test@123
+
+---
+
+ - **username:** user3@test.com
+ - **password:** Test@123
+
+Each user has access to different workspaces with the following roles:
+
+- user1@test.com
+  - Workspace 01 with admin role
+  - Workspace 02 with admin role
+  - Workspace 03 with admin role
+
+- user2@test.com
+  - Workspace 01 with admin role
+  - Workspace 02 with resource manager role
+
+- user3@test.com
+  - Workspace 04 with admin role
+
+
---
+++ src/view/auth/PasswordChangeFormPage.tsx
@@ -0,0 +1,37 @@
+import { i18n } from 'src/i18n';
+import { getHistory } from 'src/modules/store';
+import PasswordChangeForm from 'src/view/auth/PasswordChangeForm';
+import ContentWrapper from 'src/view/layout/styles/ContentWrapper';
+import Breadcrumb from 'src/view/shared/Breadcrumb';
+import PageTitle from 'src/view/shared/styles/PageTitle';
+
+
+const doNavigateToDashboard = () => {
+  getHistory().push('/');
+};
+
+
+function PasswordChangeFormPage(props) {
+  return (
+    <>
+      <Breadcrumb
+        items={[
+          [i18n('dashboard.menu'), '/'],
+          [i18n('auth.passwordChange.title')],
+        ]}
+      />
+
+      <ContentWrapper>
+        <PageTitle>
+          {i18n('auth.passwordChange.title')}
+        </PageTitle>
+
+        <PasswordChangeForm
+          onCancel={doNavigateToDashboard}
+        />
+      </ContentWrapper>
+    </>
+  );
+}
+
+export default PasswordChangeFormPage;
